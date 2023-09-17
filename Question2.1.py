4528b8faf47feeafb59fa3caa19416cbf56a70b7
Eren <143909674+erengmsj@users.noreply.github.com>
2023-09-17 15:11:41 +0300

    BMI Calculator

---
+++ Question2.1.py
@@ -0,0 +1,16 @@
+boy = float(input("Boyunuz:"))
+kilo = int(input("Kilonuz:"))
+
+bki = kilo / (boy ** 2)
+
+if (bki < 18.5):
+    print("Zayıf")
+
+elif (bki >= 18.5 and bki < 25):
+    print("Normal")
+
+elif (bki >= 25 and bki < 30):
+    print("Fazla Kilolu")
+
+else:
+    print("Obez")
