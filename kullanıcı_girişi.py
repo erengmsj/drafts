b726e2289f581742bb728a47fe59199cf50f166b
Eren <143909674+erengmsj@users.noreply.github.com>
2023-09-18 12:30:52 +0300

    kullanıcı_girişi.py

--- kullanıcı_girişi.py
+++ kullanıcı_girişi.py
@@ -7,17 +7,27 @@ Kullanıcı Girişi
 sys_kullanıcı_adı = "erenn"
 sys_parola = "12345"
 
-kullanıcı_adı = input("Kullanıcı Adı:")
-parola = input("Parola:")
+giriş_hakkı = 3
 
-if (kullanıcı_adı == sys_kullanıcı_adı and sys_parola != parola):
-    print("Parola Hatalı..")
+while True:
+    kullanıcı_adı = input("Kullanıcı Adı:")
+    parola = input("Parola:")
 
-elif (kullanıcı_adı != sys_kullanıcı_adı and sys_parola == parola):
-        print("Kullanıcı Adı Hatalı..")
+    if (kullanıcı_adı == sys_kullanıcı_adı and sys_parola != parola):
+        print("Parola Hatalı..")
+        giriş_hakkı -= 1
 
-elif (kullanıcı_adı != sys_kullanıcı_adı and sys_parola != parola):
-        print("Kullanıcı Adı ve Parola Hatalı..")
+    elif (kullanıcı_adı != sys_kullanıcı_adı and sys_parola == parola):
+            print("Kullanıcı Adı Hatalı..")
+            giriş_hakkı -= 1
 
-else:
-    print("Sisteme Başarıyla Giriş Yaptınız")
+    elif (kullanıcı_adı != sys_kullanıcı_adı and sys_parola != parola):
+            print("Kullanıcı Adı ve Parola Hatalı..")
+            giriş_hakkı -= 1
+
+    else:
+        print("Sisteme Başarıyla Giriş Yaptınız")
+        break
+    if (giriş_hakkı == 0):
+        print("Giriş Hakkınız Bitti..")
+        break
